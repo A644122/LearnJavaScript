6246f6a9f9e56ffadcd454748a051c3bd25337b8
alex <mdafrozalam109@gmail.com>
2023-08-25 19:24:52 +0530

    import kia mai file2 ko learnjavascript me

---
+++ File2/main.js
@@ -0,0 +1,15 @@
+const div=document.querySelector('#para');
+
+const div1=document.querySelector('div');
+
+
+// console.log(div);
+// div.setAttribute('id','newid');
+
+div1.getAttribute('class',);
+div1.setAttribute('class','list-1','afroz');
+console.log(div1)
+
+let h1=document.querySelector('h1');
+console.log(h1);
+h1.setAttribute('h1')

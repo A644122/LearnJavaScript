0d98614e5549e7ceece413a5ffdf38234e4eea02
alex <mdafrozalam109@gmail.com>
2023-08-25 19:18:06 +0530

    learning jsvascript basic

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// 'use strict'//isme ham ko batana hoga ke koun sa datatype hai tavi use strict run hoga //
+// const a=10;
+// console.log(a);
+
+
+
+// b='hellow world'
+//  let b='hellow world'; //example of use strict//
+// console.log(b);
+
+//String//
+let str='';
+let str1='A';
+let str2='Hwllo world';
+let str3=10;
+console.log(typeof str);
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof str3);
+
+
+//UNDEFINE//
+let e=undefined;
+console.log(typeof e);
+
+//OBJECT//
+let k=null;
+console.log(typeof k);
+
+
